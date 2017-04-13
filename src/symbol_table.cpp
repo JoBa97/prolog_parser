bf8ad8bb30afa66f0cf9d717b1610cc3a99dfedd
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-04-13 16:20:21 +0200

    print cleaner

--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -11,21 +11,19 @@ void print_symbol_table(symbol_table_t& symbol_table){
   for(auto& statement_info: symbol_table) {
     std::cout << "  {" << std::endl;
     for(auto& lit_info: statement_info) {
-      std::cout << "    {" << std::endl;
-      std::cout << "      " << lit_info.first.repr() << " -> " << std::endl;
+      std::cout << "    " << lit_info.first.repr() << " -> " << std::endl;
+      std::cout << "      [" << std::endl;
       std::cout << "        [" << std::endl;
-      std::cout << "          [" << std::endl;
       for(auto& var_id: lit_info.second.first) {
-        std::cout << "            " << var_id.repr() << "," << std::endl;
+        std::cout << "          " << var_id.repr() << "," << std::endl;
       }
-      std::cout << "          ]," << std::endl;
-      std::cout << "          [" << std::endl;
+      std::cout << "        ]," << std::endl;
+      std::cout << "        [" << std::endl;
       for(auto& const_id: lit_info.second.second) {
-        std::cout << "            " << const_id.repr() << "," << std::endl;
+        std::cout << "          " << const_id.repr() << "," << std::endl;
       }
-      std::cout << "          ]," << std::endl;
-      std::cout << "        ]" << std::endl;
-      std::cout << "    }," << std::endl;
+      std::cout << "        ]," << std::endl;
+      std::cout << "      ]," << std::endl;
     }
     std::cout << "  }, " << std::endl;
   }
